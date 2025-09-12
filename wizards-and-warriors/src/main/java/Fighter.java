f55cb4daa884c4c66049f51bedbec4056161b002
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-11 22:27:39 -0300

    fim wizards-and-warriors

--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -11,4 +11,60 @@ class Fighter {
 
 // TODO: define the Warrior class
 
+class Warrior extends Fighter {
+
+    public String toString() {
+
+        return "Fighter is a Warrior";
+
+    }
+
+    @Override
+    public boolean isVulnerable() {
+
+        return false;
+
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+
+        return fighter.isVulnerable() ? 10 : 6;
+
+    }
+
+}
+
 // TODO: define the Wizard class
+
+class Wizard extends Fighter {
+
+    boolean spell;
+
+    public String toString() {
+
+        return "Fighter is a Wizard";
+
+    }
+
+    public void prepareSpell() {
+
+        spell = true;
+
+    }
+
+    @Override
+    public boolean isVulnerable() {
+
+        return !spell;
+
+    }
+
+    @Override
+    public int getDamagePoints (Fighter fighter) {
+
+        return spell ? 12 : 3;
+
+    }
+
+}
